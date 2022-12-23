767f21d185aa1bf6c97d4f007cf8a0d8500ef18c
namtong36 <tong.hai.nam@sun-asterisk.com>
2022-12-23 17:39:21 +0700

    develop

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div className="relative h-[140vh]">
+      <Head>
+        <title>Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+      <main>
+         {/* Banner */}
+         <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+         </section>
+         {/* Modal */}
+      </main>
+    </div>
+  )
+}
+
+export default Home
