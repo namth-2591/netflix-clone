be1aa73c88aa81c2e26339d1af9d26e8da1b3a29
namtong36 <tong.hai.nam@sun-asterisk.com>
2022-12-26 16:43:15 +0700

    add typing file

--- components/Header.tsx
+++ components/Header.tsx
@@ -38,7 +38,7 @@ function Header() {
         </div>
         <div className="flex items-center space-x-4 text-sm font-light">
           <SearchIcon className="hidden sm:inline h-6 w-6" />
-          <p className="hidden lg-inline">Kids</p>
+          <p className="hidden lg:inline">Kids</p>
           <BellIcon className="h-6 w-6" />
           <Link href="/account">
             <img src="https://rb.gy/g1pwyx"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,26 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
+import requests from "../utils/requests";
+import { Movie } from "../typings";
 
-const Home: NextPage = () => {
+interface Props{
+  netflixOriginals: Movie[],
+
+}
+
+const Home = ({ netflixOriginals }: Props) => {
+  
   return (
-    <div className="relative h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header/>
       <main>
-         {/* Banner */}
+        <Banner />
          <section>
           {/* Row */}
           {/* Row */}
@@ -28,3 +37,42 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+
+
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentories
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentories: documentories.results,
+    }
+  }
+
+
+}
